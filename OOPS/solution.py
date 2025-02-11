09008effd14b24be284f6fc47138f50fcefc3797
Nayon Mazumder <nayonmazumder4@gmail.com>
2025-02-11 17:13:29 +0600

    Add soultion of all problem

--- OOPS/solution.py
+++ OOPS/solution.py
@@ -6,8 +6,11 @@ class Car:                 # Class and Object Exmaple
     def FullName(self):
         return f"Full Details : {self.__brand} {self.model}"
 
-    def get_brand(self):                    # Encapsulation!
+    def get_brand(self):                # Encapsulation!
         return f"{self.__brand} is your Brand !"
+
+    def fuel_type(self):                # Polymorphisms
+        return "Disel or Petrol"
 
 
 class ElectricCar(Car):  # Inheritances Example
@@ -15,8 +18,17 @@ class ElectricCar(Car):  # Inheritances Example
         super().__init__(brand, model)
         self.battery_size = battery_size
 
+    def fuel_type(self):                # Polymorphisms
+        return "Electric Charge"
+
+
+my_Tata = Car("TATA", "Safari")
+print(my_Tata.FullName())
+print(my_Tata.get_brand())
+print(my_Tata.fuel_type())
 
 my_New_car = ElectricCar("Telsa", "Model S", "85 kWh")
 print(my_New_car.get_brand())
 print(my_New_car.FullName())
 print(my_New_car.battery_size)
+print(my_New_car.fuel_type())
